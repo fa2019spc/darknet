f7117e3216adab6f6beac00b07821221af573562
ie18sugi <ie18sugi@yamagata-cit.ac.jp>
2020-01-22 09:53:40 +0900

    Initial commit

--- python/divide.py
+++ python/divide.py
@@ -5,15 +5,15 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 
 print(current_dir)
 
-current_dir = '../data/azi'
-g_colab_full_path='/content/darknet/data/azi'
+current_dir = '../data/isw'
+g_colab_full_path='/content/darknet/data/isw'
 
 # Percentage of images to be used for the test set
 percentage_test = 10;
 
 # Create and/or truncate train.txt and test.txt
-file_train = open('azi-train.txt', 'w')  
-file_test = open('azi-test.txt', 'w')# Populate train.txt and test.txt
+file_train = open('isw-train.txt', 'w')  
+file_test = open('isw-test.txt', 'w')# Populate train.txt and test.txt
 counter = 1  
 index_test = round(100 / percentage_test)  
 for pathAndFilename in glob.iglob(os.path.join(current_dir, "*.jpg")):  
